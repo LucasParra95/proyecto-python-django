35cd9feba7dd24381b2f13a3c9492fb627e17fb5
LucasParra95 <lucasandresparra@gmail.com>
2026-08-25 20:41:55 -0300

    Modelo Cliente

--- servicios/models.py
+++ servicios/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    """Modelo para gestionar clientes"""
+
+    nombre = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        help_text="Nombre del cliente"
+    )
+
+    apellido = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        help_text="Apellido del cliente"
+    )
+
+    activo = models.BooleanField(
+        default=True,
+        help_text="Indica si el cliente está activo"
+    )
