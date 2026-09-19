6dadc5d85010994c29ecf2e6da22bc9411b9a0bd
LucasParra95 <lucasandresparra@gmail.com>
2026-09-19 10:55:09 -0300

    Test

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('servicios/', include("servicios.urls")),
     path('api/', include("api.urls")),
